12fb0ef7328c8b827d60858a1b176323c45d3990
benedikt.grimmeisen <b.g@test.de>
2018-12-12 18:10:58 +0100

    added empty application

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'studentmanager.apps.StudentmanagerConfig',
 ]
 
 MIDDLEWARE = [
